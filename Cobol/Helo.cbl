5cc18bd209d8013a8d12a7c03c5349c302361911
Dhinesh Palanisamy <dhinesh.palanisamy@tcs.com>
2020-07-21 01:40:54 -0500

    fourth

--- Cobol/Helo.cbl
+++ Cobol/Helo.cbl
@@ -2,5 +2,5 @@ IDENTIFICATION DIVISION.
 PROGRAM-ID. HELLO-WORLD.
 * simple hello world program
 PROCEDURE DIVISION.
-    DISPLAY 'Hello dhinesh palanisamy'.
+    DISPLAY 'Hello dhinesh palani'.
     STOP RUN.
